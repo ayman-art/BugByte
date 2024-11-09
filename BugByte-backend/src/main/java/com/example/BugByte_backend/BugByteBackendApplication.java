58f533e264d990c83188e5cc788e4668ba77d8fe
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-09 20:34:01 +0200

    Initializing Spring boot project

---
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/BugByteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BugByte_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugByteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugByteBackendApplication.class, args);
+	}
+
+}
---
+++ BugByte-backend/src/test/java/com/example/BugByte_backend/BugByteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BugByte_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugByteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
