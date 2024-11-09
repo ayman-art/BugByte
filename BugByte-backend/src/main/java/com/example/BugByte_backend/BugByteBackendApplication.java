a5c53f8be231ab0db3e8b5c90a587fde03a5ecdb
Porvah <3msabdullah@gmail.com>
2024-11-09 23:47:07 +0200

    jwt basis todo: testing

--- BugByte-backend/src/main/java/com/example/BugByte_backend/BugByteBackendApplication.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/BugByteBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.BugByte_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BugByteBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BugByteBackendApplication.class, args);
 	}
 
