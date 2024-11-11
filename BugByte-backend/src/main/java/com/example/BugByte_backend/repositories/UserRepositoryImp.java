6ace20aec1a49e09d6a8a9c98810d200125efafb
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-11 05:51:09 +0200

    Adding queries and methods for finding by id, identity and password

--- BugByte-backend/src/main/java/com/example/BugByte_backend/repositories/UserRepositoryImp.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/repositories/UserRepositoryImp.java
@@ -3,6 +3,7 @@ package com.example.BugByte_backend.repositories;
 import com.example.BugByte_backend.models.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -17,6 +18,8 @@ public class UserRepositoryImp implements UserRepository {
             """;
     private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM users WHERE email = ?;";
     private static final String SQL_COUNT_BY_USERNAME = "SELECT COUNT(*) FROM users WHERE user_name = ?;";
+    private static final String SQL_FIND_BY_ID = "SELECT * FROM users WHERE id = ?;";
+    private static final String SQL_FIND_BY_IDENTITY = "SELECT * FROM users WHERE email = ? OR user_name = ?;";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -32,6 +35,9 @@ public class UserRepositoryImp implements UserRepository {
 
     @Override
     public UserModel findByIdentityAndPassword(String identity, String password) {
+        UserModel user = jdbcTemplate.queryForObject(SQL_FIND_BY_IDENTITY, userRowMapper, identity, identity);
+        if (user != null && passwordEncoder.matches(password, user.getPassword()))
+            return user;
         return null;
     }
 
@@ -47,7 +53,7 @@ public class UserRepositoryImp implements UserRepository {
 
     @Override
     public UserModel findById(Integer userId) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, userRowMapper, userId);
     }
 
     @Override
@@ -64,4 +70,13 @@ public class UserRepositoryImp implements UserRepository {
     public Boolean makeUserAdmin(Integer userId) {
         return null;
     }
+
+    private final RowMapper<UserModel> userRowMapper = ((rs, rowNum) -> new UserModel(
+            rs.getLong("id"),
+            rs.getString("user_name"),
+            rs.getString("email"),
+            rs.getString("password"),
+            rs.getLong("reputation"),
+            rs.getBoolean("is_admin")
+    ));
 }
