4c2f2e7e49fe818d8140678c3ce455312ba5c33b
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-25 11:38:42 +0200

    Fixing indentations in RegistrationService class.

--- BugByte-backend/src/main/java/com/example/BugByte_backend/services/RegistrationService.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/services/RegistrationService.java
@@ -14,82 +14,78 @@ public class RegistrationService {
 
     EmailService emailService = new EmailService();
 
-    public User registerUser(String email , String userName , String password) throws Exception{
+    public User registerUser(String email, String userName, String password) throws Exception {
         try {
-             if(!registrationCOR.validateEmail(email)){
-                 throw new Exception("email is not valid");
-             }
-             if(!registrationCOR.validateUserName(userName)){
-                 throw new Exception("userName is too short");
-             }
-             if(!registrationCOR.validatePassword(password)){
-                 throw new Exception("week password");
-             }
+            if(!registrationCOR.validateEmail(email)){
+                throw new Exception("email is not valid");
+            }
+            if(!registrationCOR.validateUserName(userName)){
+                throw new Exception("userName is too short");
+            }
+            if(!registrationCOR.validatePassword(password)){
+                throw new Exception("week password");
+            }
             //insert user in the database
-            long id  = userRepository.insertUser(userName , email , password);
-             return new User(id, userName, email, password);
+            long id  = userRepository.insertUser(userName, email, password);
+            return new User(id, userName, email, password);
         }
         catch (Exception e){
             throw new Exception("Error registering user , user Already exists: " + e.getMessage());
         }
     }
 
-    public User loginUser(String identity , String password) throws Exception{
+    public User loginUser(String identity, String password) throws Exception {
         try {
-
             //get the user from the database
-                User user = userRepository.findByIdentityAndPassword(identity , password);
-                if(user == null){
-                    throw new NullPointerException("User doesn't exist");
-                }
-                return user;
+            User user = userRepository.findByIdentityAndPassword(identity, password);
+            if(user == null) {
+                throw new NullPointerException("User doesn't exist");
+            }
+            return user;
         }
-        catch (Exception e){
+        catch (Exception e) {
             throw new Exception("Error occurred while logging in user: " + e.getMessage());
         }
     }
 
-
-    public User logoutUser(long userId) throws Exception{
+    public User logoutUser(long userId) throws Exception {
         try {
             //check if the user exists in the database
             User user = userRepository.findById(userId);
             if (user == null) {
                 throw new Exception("User doesn't exist");
             }
-                return user;
+            return user;
         }
-        catch (Exception e){
+        catch (Exception e) {
             throw new Exception("Error occurred while logging out user:  " + e.getMessage());
         }
     }
 
-
-    public boolean deleteUser(long userId) throws Exception{
+    public boolean deleteUser(long userId) throws Exception {
         try {
             //check if the user exists in the database
             User user = userRepository.findById(userId);
             if (user == null) {
                 throw new Exception("User doesn't exist");
             }
-                //delete the user
-                return userRepository.deleteUser(userId);
+            //delete the user
+            return userRepository.deleteUser(userId);
         }
         catch (Exception e){
             throw new Exception("Error occurred while deleting user:  " + e.getMessage());
         }
     }
 
-
-    public boolean changePassword(long userId , String newPassword) throws Exception{
+    public boolean changePassword(long userId, String newPassword) throws Exception {
         try {
             //check if the user exists in the database
             User user = userRepository.findById(userId);
             if (user == null) {
                 throw new Exception("User doesn't exist");
             }
-                //change the password
-                return userRepository.changePassword(userId , newPassword);
+            //change the password
+            return userRepository.changePassword(userId, newPassword);
         }
         catch (Exception e){
             throw new Exception("Error occurred while changing password:  " + e.getMessage());
