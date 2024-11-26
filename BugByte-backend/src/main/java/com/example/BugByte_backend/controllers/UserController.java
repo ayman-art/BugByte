84e6f2d3d50d77e83f6f532597222488a78bf125
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-27 00:39:02 +0200

    Creating skeleton methods for get and update profile

--- BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
@@ -1,10 +1,44 @@
 package com.example.BugByte_backend.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    // Auto wiring the administrative facade
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+        token = token.replace("Bearer ", "");
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        userData.put("userid", userid);
+        try {
+            // Administrative facade calling getProfile method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping("/update-profile")
+    public ResponseEntity<?> updateProfile(@RequestHeader("Authorization") String token, @RequestBody Map<String, Object> userData) {
+        token = token.replace("Bearer ", "");
+        userData.put("jwt", token);
+        try {
+            // Administrative facade calling getProfile method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+
 
 }
