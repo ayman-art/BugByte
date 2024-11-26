ff59220007b4c2094edcf9f3b9d9d197603bffca
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-27 00:46:28 +0200

    Creating skeleton methods for follow, unfollow, getFollowings, and make admin methods.

--- BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
         userData.put("jwt", token);
         userData.put("userid", userid);
         try {
-            // Administrative facade calling getProfile method
+            // Administrative facade calling get profile method
             return null;
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
@@ -32,13 +32,65 @@ public class UserController {
         token = token.replace("Bearer ", "");
         userData.put("jwt", token);
         try {
-            // Administrative facade calling getProfile method
+            // Administrative facade calling update profile method
             return null;
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
+    @PutMapping("/follow")
+    public ResponseEntity<?> followUser(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+        token = token.replace("Bearer ", "");
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        userData.put("userid", userid);
+        try {
+            // Administrative facade calling follow method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
 
+    @DeleteMapping("/unfollow")
+    public ResponseEntity<?> unFollowUser(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+        token = token.replace("Bearer ", "");
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        userData.put("userid", userid);
+        try {
+            // Administrative facade calling unfollow method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
 
+    @GetMapping("/following")
+    public ResponseEntity<?> getFollowings(@RequestHeader("Authorization") String token) {
+        token = token.replace("Bearer ", "");
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        try {
+            // Administrative facade calling get followings method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping("/make-admin")
+    public ResponseEntity<?> makeAdmin(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+        token = token.replace("Bearer ", "");
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        userData.put("userid", userid);
+        try {
+            // Administrative facade calling make admin method
+            return null;
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
