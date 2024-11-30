7f6c56d8f55f586c36a7541119b3360d66b87b4a
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-11-30 20:26:29 +0200

    Calling administrative facade methods inside the controllers methods and returning the response

--- BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/controllers/UserController.java
@@ -1,27 +1,34 @@
 package com.example.BugByte_backend.controllers;
 
+import com.example.BugByte_backend.facades.AdministrativeFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
-    // Auto wiring the administrative facade
+    @Autowired
+    private AdministrativeFacade administrativeFacade;
 
     @GetMapping("/profile")
-    public ResponseEntity<?> getProfile(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+    public ResponseEntity<?> getProfile(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
+        System.out.println("HERE");
         token = token.replace("Bearer ", "");
+
         Map<String, Object> userData = new HashMap<>();
         userData.put("jwt", token);
-        userData.put("userid", userid);
+        userData.put("user-name", username);
+
         try {
-            // Administrative facade calling get profile method
-            return null;
+            Map<String, Object> response = administrativeFacade.getProfile(userData);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
@@ -40,55 +47,92 @@ public class UserController {
     }
 
     @PutMapping("/follow")
-    public ResponseEntity<?> followUser(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+    public ResponseEntity<?> followUser(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
         token = token.replace("Bearer ", "");
+
         Map<String, Object> userData = new HashMap<>();
         userData.put("jwt", token);
-        userData.put("userid", userid);
+        userData.put("user-name", username);
+
         try {
-            // Administrative facade calling follow method
-            return null;
+            boolean res = administrativeFacade.followUser(userData);
+
+            if (res)
+                return new ResponseEntity<>("User followed successfully.", HttpStatus.OK);
+            else
+                return new ResponseEntity<>("Failed to follow the user.", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @DeleteMapping("/unfollow")
-    public ResponseEntity<?> unFollowUser(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+    public ResponseEntity<?> unFollowUser(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
         token = token.replace("Bearer ", "");
+
         Map<String, Object> userData = new HashMap<>();
         userData.put("jwt", token);
-        userData.put("userid", userid);
+        userData.put("user-name", username);
+
         try {
-            // Administrative facade calling unfollow method
-            return null;
+            boolean res = administrativeFacade.unfollowUser(userData);
+
+            if (res)
+                return new ResponseEntity<>("User unfollowed successfully.", HttpStatus.OK);
+            else
+                return new ResponseEntity<>("Failed to unfollow the user.", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/following")
-    public ResponseEntity<?> getFollowings(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<?> getFollowings(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
         token = token.replace("Bearer ", "");
+
         Map<String, Object> userData = new HashMap<>();
         userData.put("jwt", token);
+        userData.put("user-name", username);
+
         try {
-            // Administrative facade calling get followings method
-            return null;
+            List<Map<String, Object>> res = administrativeFacade.getFollowings(userData);
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("/followers")
+    public ResponseEntity<?> getFollowers(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
+        token = token.replace("Bearer ", "");
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("jwt", token);
+        userData.put("user-name", username);
+
+        try {
+            List<Map<String, Object>> res = administrativeFacade.getFollowers(userData);
+            return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @PutMapping("/make-admin")
-    public ResponseEntity<?> makeAdmin(@RequestHeader("Authorization") String token, @RequestParam("userid") Long userid) {
+    public ResponseEntity<?> makeAdmin(@RequestHeader("Authorization") String token, @RequestParam("username") String username) {
         token = token.replace("Bearer ", "");
+
         Map<String, Object> userData = new HashMap<>();
         userData.put("jwt", token);
-        userData.put("userid", userid);
+        userData.put("user-name", username);
+
         try {
-            // Administrative facade calling make admin method
-            return null;
+            boolean res = administrativeFacade.makeAdmin(userData);
+
+            if (res)
+                return new ResponseEntity<>("User promoted to admin successfully.", HttpStatus.OK);
+            else
+                return new ResponseEntity<>("Failed to promote user to admin.", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
