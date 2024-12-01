b1ea78dee69d5ec5f80977fd715ac4eee097605e
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-12-01 18:49:52 +0200

    edit

--- bugbyte/src/API/ApiUrls.tsx
+++ bugbyte/src/API/ApiUrls.tsx
@@ -7,6 +7,5 @@ export const API_URLS = {
   LOGIN_BY_GOOGLE:`https://www.googleapis.com/oauth2/v3/userinfo`,
   FORGOT_PASSWORD:`${API_BASE_URL}/users/reset-password`,
   VALIDATE_CODE: `${API_BASE_URL}/users/validate-email-code`,
-  RESEND_CODE: `${API_BASE_URL}/users/resendCode`,
   CHANGE_PASSWORD: `${API_BASE_URL}/users/change-password`,
 };
