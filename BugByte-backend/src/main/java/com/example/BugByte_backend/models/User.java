166994227f6500bb33d61d9124a9a5d25cb7ce9f
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-08 00:50:58 +0200

    Using lombok getters and setters.

--- BugByte-backend/src/main/java/com/example/BugByte_backend/models/User.java
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/models/User.java
@@ -2,9 +2,17 @@ package com.example.BugByte_backend.models;
 
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,10 +35,6 @@ public class User {
 
     @Column(nullable = false)
     private Boolean isAdmin;
-
-    public User() {
-        this(0L, "", "", "", 0L, false);
-    }
 
     public User(String userName, String email, String password) {
         this(0L, userName, email, password, "", 0L, false);
@@ -56,61 +60,5 @@ public class User {
         this.bio = bio;
         this.reputation = reputation;
         this.isAdmin = isAdmin;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public Long getReputation() {
-        return reputation;
-    }
-
-    public void setReputation(Long reputation) {
-        this.reputation = reputation;
-    }
-
-    public Boolean getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setIsAdmin(Boolean is_admin) {
-        this.isAdmin = is_admin;
     }
 }
