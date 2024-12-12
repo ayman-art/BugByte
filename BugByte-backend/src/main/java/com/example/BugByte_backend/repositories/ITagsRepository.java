6060ebeedead17bf9700e4d048fcaa0340015836
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-12 14:43:33 +0200

    Creating ITagsRepository interface with the required methods.

---
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/repositories/ITagsRepository.java
@@ -0,0 +1,12 @@
+package com.example.BugByte_backend.repositories;
+
+import java.util.List;
+
+public interface ITagsRepository {
+
+    Long insertTag(String name);
+
+    Long bulkAddTagsToQuestion(Long questionId, List<String> tags);
+
+    Boolean removeTagsFromQuestion(Long questionId);
+}
