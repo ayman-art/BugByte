50f0801cff6af8f098cee03645ae407b4f1f0afb
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-13 16:31:17 +0200

    Created Searching filtering service for questions

---
+++ BugByte-backend/src/main/java/com/example/BugByte_backend/services/SearchingFilteringQuestionService.java
@@ -0,0 +1,38 @@
+package com.example.BugByte_backend.services;
+
+import com.example.BugByte_backend.models.Question;
+import com.example.BugByte_backend.repositories.SearchingFilteringQuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class SearchingFilteringQuestionService {
+    @Autowired
+    private SearchingFilteringQuestionRepository questionRepository;
+
+    public Question saveQuestion(Question question) {
+        if (question == null)
+            throw new NullPointerException("Question can't be null");
+        return questionRepository.save(question);
+    }
+
+    public Page<Question> searchQuestions(String query, int page, int size) {
+        if (query == null)
+            throw new NullPointerException("Query can't be null");
+
+        Pageable pageable = PageRequest.of(page, size);
+        return questionRepository.findByMdContentAndTags(query, pageable);
+    }
+
+    public List<Question> getQuestionsByTags(List<String> tags) {
+        if (tags == null || tags.isEmpty())
+            throw new NullPointerException("tags can't be null or empty");
+
+        return questionRepository.findByTagsIn(tags);
+    }
+}
