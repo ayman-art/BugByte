7faa961e89b7e5a411bf2995741e6fca7d8b5e93
Porvah <3msabdullah@gmail.com>
2024-12-15 23:58:17 +0200

    created empty community page

--- bugbyte/src/App.tsx
+++ bugbyte/src/App.tsx
@@ -7,6 +7,7 @@ import ProfilePage from './pages/ProfilePage';
 import { authorizeToken, saveData } from './API/HomeAPI';
 import Layout from './layouts/MainLayout';
 import TestPage from './pages/TestPage';
+import CommunityPage from './pages/CommunityPage';
 
 const isLoggedIn = false;
 
@@ -65,7 +66,7 @@ const App: React.FC = () => {
             <Route path="/LogIn" element={<Login onLogin={handleLogin}/>} />
           </>
         )}
-        <Route path="testPage" element={<Layout onLogout={handleLogout}><TestPage/></Layout>}/>
+        <Route path="testPage" element={<Layout onLogout={handleLogout}><CommunityPage/></Layout>}/>
       </Routes>
     </BrowserRouter>
       );
---
+++ bugbyte/src/pages/CommunityPage.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import MDEditor from '../components/MDEditor';
+import MDViewer from '../components/MDViewer';
+
+const CommunityPage: React.FC = () => {
+    
+
+    return (
+        <div>
+        </div>
+      );
+};
+
+export default CommunityPage;
