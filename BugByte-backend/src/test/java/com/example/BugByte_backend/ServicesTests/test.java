6aab63c7b0a68cc9dabb2714bf42320b88573433
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-12-16 17:20:17 +0200

    .

--- BugByte-backend/src/test/java/com/example/BugByte_backend/ServicesTests/test.java
+++ BugByte-backend/src/test/java/com/example/BugByte_backend/ServicesTests/test.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class test {
@@ -28,6 +29,22 @@ public class test {
 //        tags.add("1");
         question.setTags(tags);
         long id = postingService.postQuestion(question);
+        System.out.println(id);
+
+    }
+    @Test
+    public void testfun2() throws Exception {
+        Question question = new Question();
+        question.setTitle("hi");
+        question.setCommunityId(1L);
+        question.setCreatorUserName("RowanYoussef");
+        question.setMdContent("hello");
+        ArrayList<String> tags = new ArrayList<>();
+//        tags.add("1");
+        question.setTags(tags);
+        List<Question> questions = postingService.getUserQuestions("RowanYoussef" , 10 , 0);
+        System.out.println(questions.get(0).getMdContent());
+        Question id = postingService.getQuestion(21L);
         System.out.println(id);
 
     }
