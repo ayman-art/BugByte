9889a433e59d591690c2b74529e36cac621923cb
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-31 16:24:40 +0200

    Adding searching in joined communities page

--- bugbyte/src/pages/JoinedCommunititesPage.tsx
+++ bugbyte/src/pages/JoinedCommunititesPage.tsx
@@ -5,12 +5,12 @@ import CommunityPreview from "../components/CommunityPreview.tsx";
 
 const JoinedCommunitiesPage: React.FC = () => {
   const [joinedCommunities, setJoinedCommunities] = useState<Community[]>([]);
+  const [searchValue, setSearchValue] = useState<string>("");
 
   const fetchJoinedComm = async () => {
     try {
       let comms = await fetchJoinedCommunities();
       comms.reverse();
-
       setJoinedCommunities(comms);
     } catch (e) {
       console.log(e);
@@ -21,9 +21,31 @@ const JoinedCommunitiesPage: React.FC = () => {
     fetchJoinedComm();
   }, []);
 
+  // Filter communities based on search input
+  const filteredCommunities = joinedCommunities.filter(
+    (comm) => comm.name.toLowerCase().includes(searchValue.toLowerCase()) // Assuming community has a 'name' property
+  );
+
+  // Handle input change for search bar
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div style={styles.container}>
-      {joinedCommunities.map((comm) => (
+      {/* Search Section */}
+      <div style={styles.searchSection}>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleSearchChange}
+          placeholder="Search joined communities..."
+          style={styles.searchInput}
+        />
+      </div>
+
+      {/* Display filtered communities */}
+      {filteredCommunities.map((comm) => (
         <CommunityPreview key={comm.id} community={comm} />
       ))}
     </div>
@@ -37,6 +59,20 @@ const styles: { [key: string]: React.CSSProperties } = {
     alignItems: "center",
     marginTop: "20px",
     width: "100%",
+  },
+  searchSection: {
+    marginBottom: "20px",
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+  },
+  searchInput: {
+    padding: "8px",
+    marginRight: "8px",
+    borderRadius: "4px",
+    border: "2px solid #ccc",
+    width: "85%",
+    height: "50px",
   },
 };
 
